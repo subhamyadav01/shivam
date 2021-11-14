613e0e58acef29400b760fae092f0dc3cd025969
subhamyadav01 <subham.yadav_cs20@gla.ac.in>
2021-11-14 23:24:59 +0530

    initial commit

---
+++ src/test78.java
@@ -0,0 +1,23 @@
+public class test78 {
+    public static void main(String[] args) {
+        int value;
+//        Integer i1= 12;
+//        int rs=12;
+//        int ss= i1.intValue();
+      //  System.out.println(ss+12);
+//        String str = i1.toString();
+//        System.out.println(str+2);
+//        Integer i2=44;
+//        System.out.println(i1+i2);
+// String s="33";
+// int i =Integer.parseInt(s);
+//        System.out.println(i+5);
+
+      Integer i1= 55;
+      Integer i2=55;
+      System.out.println(i1.equals(i2));
+        System.out.println(i1==i2);
+        System.out.println(55==56);
+
+    }
+}
