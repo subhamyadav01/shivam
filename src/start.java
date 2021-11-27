a5501f6d24fc772fa9effa8be3e331d1ca86f67b
subhamyadav01 <subham.yadav_cs20@gla.ac.in>
2021-11-27 16:46:24 +0530

    initial commit

---
+++ src/start.java
@@ -0,0 +1,33 @@
+public class start {
+    private int roll;
+    private String name;
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setName() {
+        name = "ll";
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll() {
+        roll = 55;
+    }
+
+}
+    class love{
+    public static void main(String[] args) {
+        start obj=new start();
+       obj.setRoll();
+     obj. setName();
+        System.out.println(obj.getName()+"  "+obj.getRoll());
+        System.out.println("\n\"my name shubham");
+
+    }
+}
