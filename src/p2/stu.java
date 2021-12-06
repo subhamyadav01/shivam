728be2afc9ccee148cdc4c66321015f08e09aa0f
subhamyadav01 <subham.yadav_cs20@gla.ac.in>
2021-12-07 01:11:54 +0530

    initial commit

---
+++ src/p2/stu.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class stu extends person{
+    int mark;
+    int roll;
+
+    public stu(int age, String name, int mark, int roll) {
+        super(age, name);
+        this.mark = mark;
+        this.roll = roll;
+    }
+}
